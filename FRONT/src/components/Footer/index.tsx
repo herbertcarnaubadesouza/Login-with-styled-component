f440c53d7c3eefaacd6086e755ac11a4011d03ce
Thiago Henrique Xavier Medeiros <thihxm@gmail.com>
2022-03-27 11:02:42 -0300

    feat: Footer component

--- FRONT/src/components/Footer/index.tsx
+++ FRONT/src/components/Footer/index.tsx
@@ -1,8 +1,46 @@
-import React from 'react'
+import Link from 'next/link'
+import { GrFacebookOption, GrInstagram, GrTwitter } from 'react-icons/gr'
 import { Container } from './styles'
 
 export function Footer() {
   return (
-    <Container>Footer</Container>
+    <Container>
+      <nav className="siteLinks">
+        <ul>
+          <li>
+            <Link href="#">Contato</Link>
+          </li>
+          <li>
+            <Link href="#">Sobre</Link>
+          </li>
+          <li>
+            <Link href="#">Dúvidas</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <nav className="social">
+        <Link href="">
+          <a>
+            <span>Instagram</span>
+            <GrInstagram />
+          </a>
+        </Link>
+        <Link href="">
+          <a>
+            <span>Facebook</span>
+            <GrFacebookOption />
+          </a>
+        </Link>
+        <Link href="">
+          <a>
+            <span>Twitter</span>
+            <GrTwitter />
+          </a>
+        </Link>
+      </nav>
+
+      <p>© Copyright 2022 NOME DO SITE</p>
+    </Container>
   )
 }
--- FRONT/src/components/Footer/styles.ts
+++ FRONT/src/components/Footer/styles.ts
@@ -1,5 +1,69 @@
 import styled from "styled-components";
 
 export const Container = styled.footer`
-  
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+  align-items: center;
+
+  nav.siteLinks {
+    margin-bottom: 2rem;
+
+    ul {
+      list-style: none;
+      
+      display: flex;
+      justify-content: center;
+      gap: 4rem;
+      
+      padding: 0;
+      margin: 0;
+
+      li {
+        font-size: 1.5rem;
+        line-height: 1;
+        a {
+          &:after {
+            content: "+";
+            margin-left: 1rem;
+            line-height: 1;
+          }
+
+          &:hover, &:focus {
+            cursor: pointer;
+            filter: brightness(0.7);
+          }
+        }
+
+      }
+    }
+  }
+
+  nav.social {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 0.5rem;
+
+    a {
+      display: grid;
+      place-content: center;
+
+      width: 2.5rem;
+      aspect-ratio: 1 / 1;
+
+      border-radius: 50%;
+      border: 2px solid white;
+
+      font-size: 1.2rem;
+
+      &:hover, &:focus {
+        filter: brightness(0.7);
+      }
+
+      span {
+        display: none;
+      }
+    }
+  }
 `;
